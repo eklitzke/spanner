1eafe6c3a54cc6e6a2bbc463cfe08b4334549098
Evan Klitzke <evan@eklitzke.org>
2015-04-06 19:54:05 -0700

    get stuff working

--- src/spanner.cc
+++ src/spanner.cc
@@ -12,8 +12,12 @@
 #include <boost/program_options.hpp>
 
 #include <iostream>
+#include <random>
+#include <vector>
 
 #include "../config.h"
+#include "./particle.h"
+#include "./vec.h"
 
 namespace po = boost::program_options;
 
@@ -96,13 +100,6 @@ void cairo_close_x11_surface(cairo_surface_t *sfc) {
 }
 
 
-static void turn(double v, double max, double *diff) {
-  if (v <= 0 || v >= max) {
-    *diff *= -1.0;
-  }
-}
-
-
 int main(int argc, char **argv) {
   po::options_description general("General options");
   general.add_options()
@@ -111,11 +108,19 @@ int main(int argc, char **argv) {
 
   int pause_millis;
   int window_size;
+  int num_particles;
+  double gravity;
   po::options_description windowopts("Display options");
   windowopts.add_options()
-      ("window-size", po::value<int>(&window_size)->default_value(500));
+      ("window-size", po::value<int>(&window_size)->default_value(1000));
   windowopts.add_options()
       ("pause-millis", po::value<int>(&pause_millis)->default_value(5));
+  windowopts.add_options()
+      ("n,num-particles", po::value<int>(&num_particles)->default_value(3));
+  windowopts.add_options()
+      ("g,gravity", po::value<double>(&gravity)->default_value(1));
+  windowopts.add_options()
+      ("sun-mass", po::value<double>()->default_value(10));
 
   po::options_description all("Allowed options");
   all.add(general).add(windowopts);
@@ -137,75 +142,99 @@ int main(int argc, char **argv) {
   cairo_surface_t *sfc;
   cairo_t *ctx;
   int x, y;
-  struct timespec ts = {0, 1000000 * pause_millis};
 
-  double x0 = 20, y0 = 20, x1 = 200, y1 = 400, x2 = 450, y2 = 100;
-  double dx0 = 1, dx1 = 1.5, dx2 = 2;
-  double dy0 = 2, dy1 = 1.5, dy2 = 1;
   int running;
 
   x = y = window_size;
   sfc = cairo_create_x11_surface(&x, &y);
   ctx = cairo_create(sfc);
 
-  struct timeval last_time = {0, 0};
-  struct timeval this_time = {0, 0};
-  struct timeval tdiff;
-  for (running = 1; running;) {
+  std::vector<Particle> particles;
+  {
+    std::random_device rd;
+    std::mt19937 engine(rd());
+    std::uniform_real_distribution<double> dist(-1, 1);
+
+    Particle p{{0, 0}, {0, 0}, vm["sun-mass"].as<double>()};
+    particles.push_back(p);
+    for (int i = 0; i < num_particles; i++) {
+      Particle p{{3 * dist(engine), 3 * dist(engine)}, {0, 0}, 1};
+      particles.push_back(p);
+    }
+  }
+
+  cairo_scale(ctx, window_size / 8, window_size / 8);
+  cairo_translate(ctx, 4, 4);
+
 #if 0
-    gettimeofday(&this_time, nullptr);
-    timersub(&this_time, &last_time, &tdiff);
-    last_time = this_time;
-    long millis = (tdiff.tv_sec * 1000) + tdiff.tv_usec / 1000;
-    printf("took %d millis\n", millis);
+  double cx = 0;
+  double cy = 0;
+  cairo_user_to_device(ctx, &cx, &cy);
+  std::cout << "center is " << cx << " " << cy << std::endl;
 #endif
+
+
+  for (running = 1; running;) {
+    const struct timespec ts = {0, 1000000 * pause_millis};
+    const double timescale = pause_millis / 1000.0;
+
+    for (Particle &p : particles) {
+      p.zero_force();
+      if (p.mass() == 1) {
+        for (const Particle &o : particles) {
+          p.augment_force(gravity, o);
+        }
+      }
+    }
+    for (Particle &p : particles) {
+      p.augment_velocity(timescale);
+      p.augment_position(timescale);
+      double x = p.position().x();
+      double y = p.position().y();
+      cairo_user_to_device(ctx, &x, &y);
+    }
+
     cairo_push_group(ctx);
     cairo_set_source_rgb(ctx, 1, 1, 1);
     cairo_paint(ctx);
-    cairo_move_to(ctx, x0, y0);
-    cairo_line_to(ctx, x1, y1);
-    cairo_line_to(ctx, x2, y2);
-    cairo_line_to(ctx, x0, y0);
-    cairo_set_source_rgb(ctx, 0, 0, 1);
-    cairo_fill_preserve(ctx);
-    cairo_set_line_width(ctx, 5);
-    cairo_set_source_rgb(ctx, 1, 1, 0);
-    cairo_stroke(ctx);
+
     cairo_set_source_rgb(ctx, 0, 0, 0);
-    cairo_move_to(ctx, x0, y0);
-    cairo_show_text(ctx, "P0");
-    cairo_move_to(ctx, x1, y1);
-    cairo_show_text(ctx, "P1");
-    cairo_move_to(ctx, x2, y2);
-    cairo_show_text(ctx, "P2");
+    for (const Particle &p : particles) {
+#if 0
+      double px = p.position().x();
+      double py = p.position().y();
+      cairo_user_to_device(ctx, &px, &py);
+      std::cout << "position = " << p.position() << ", device coordinates are "
+                << "(" << px << ", " << py << ")\n";
+#endif
+
+      cairo_arc(ctx,
+                p.position().x(),
+                p.position().y(),
+                sqrt(p.mass()) * 0.03,
+                0,
+                2 * M_PI);
+
+      cairo_set_line_width(ctx, 0.01);
+      cairo_stroke(ctx);
+    }
+
     cairo_pop_group_to_source(ctx);
     cairo_paint(ctx);
     cairo_surface_flush(sfc);
 
-    x0 += dx0;
-    y0 += dy0;
-    x1 += dx1;
-    y1 += dy1;
-    x2 += dx2;
-    y2 += dy2;
-    turn(x0, x, &dx0);
-    turn(x1, x, &dx1);
-    turn(x2, x, &dx2);
-    turn(y0, y, &dy0);
-    turn(y1, y, &dy1);
-    turn(y2, y, &dy2);
-
     switch (cairo_check_event(sfc, 0)) {
       case 0xff53:   // right cursor
-        dx0 *= 2.0;
-        dy0 *= 2.0;
+        if (pause_millis > 1) {
+          pause_millis -= 1;
+        }
         break;
 
       case 0xff51:   // left cursor
-        dx0 /= 2.0;
-        dy0 /= 2.0;
+        pause_millis++;
         break;
 
+      case 'q':
       case 0xff1b:   // Esc
       case -1:       // left mouse button
         running = 0;
